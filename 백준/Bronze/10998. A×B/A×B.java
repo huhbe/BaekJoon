e2e9ca6d6bc01e6ca7cfc5368a904a1fcdbbdce8
huhbe <142882186+huhbe@users.noreply.github.com>
2024-01-15 12:05:39 +0900

    [Bronze V] Title: A×B, Time: 204 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String a=sc.nextLine();
+		String[] abc=a.split(" ");
+		int result = Integer.parseInt(abc[0])*Integer.parseInt(abc[1]);
+		System.out.println(result);
+	}
+}
