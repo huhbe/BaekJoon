6202c82db57fabb36d97e165e2cb0981364417b3
huhbe <142882186+huhbe@users.noreply.github.com>
2024-01-15 11:30:41 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 224 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		String[] abc = a.split(" ");
+
+		if (Integer.parseInt(abc[0]) > Integer.parseInt(abc[1])) {
+			System.out.println(">");
+		} else if (Integer.parseInt(abc[0]) < Integer.parseInt(abc[1])) {
+			System.out.println("<");
+		} else if (Integer.parseInt(abc[0]) == Integer.parseInt(abc[1])) {
+			System.out.println("==");
+		}
+	}
+}
